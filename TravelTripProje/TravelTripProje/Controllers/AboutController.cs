e9d14040d5fe380d21750809622399efa3f0d049
Merve Lamia <mrvlmyksby@gmail.com>
2024-09-26 18:27:28 +0300

    TravelProject
    Developed with ASP.NET Core. Valid on all websites

---
+++ TravelTripProje/TravelTripProje/Controllers/AboutController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TravelTripProje.Models.Sınıflar;
+namespace TravelTripProje.Controllers
+{
+    public class AboutController : Controller
+    {
+        // GET: About
+        Context c = new Context(); //Context sınıfında c isimli nesne türettik.
+                                   //C nesnesi yardımıyla da contexte bağlı olan sınıflara ulaşıp hakkımızda tablosunu listeledik.  
+        public ActionResult Index()
+        {
+            var degerler = c.Hakkimizdas.ToList();
+            return View(degerler);
+        }
+    }
+}
---
+++ TravelTripProje/TravelTripProje/Models/Sınıflar/Blog.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TravelTripProje.Models.Sınıflar
+{
+    public class Blog
+    {
+        [Key]
+        public int ID { get; set; }
+        public string baslik { get; set; }
+        public DateTime Tarih { get; set; }
+        public string Aciklama { get; set; }
+        public string BlokImage { get; set; }
+        public ICollection<Yorumlar>Yorumlars{get;set;} //Bloglar tablomuzla yorumlar tablomuzu bir çok ilişkisini oluşturur.
+        //ICollection interfacetir. kümeleme listeleme işlemlerini yapar.
+    }
+}
